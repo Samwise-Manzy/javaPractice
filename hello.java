a72804b0c0949124b2cd5d41c97656816937a77d
Samuel Manzanares <samuel.manz57@gmail.com>
2023-05-24 12:42:56 -0500

    java progression

--- hello.java
+++ hello.java
@@ -2,19 +2,37 @@ import java.util.Scanner;
 
 public class hello {
 
-    public static void main(String[] args) {        // method named main
-        System.out.println("What is your name?");
+    public static void main(String[] args) {     
+    
+    Scanner scanner = new Scanner(System.in);
+    // String x = scanner.nextLine();
 
-        Scanner scanner = new Scanner(System.in); 
-    String name = scanner.nextLine();
+    // System.out.println(x);
 
-    System.out.println("hello " + name);
+    int y = scanner.nextInt();
+    System.out.println(y + 10);
+    int x = (int)scanner.nextDouble();  // type casting 
+    System.out.println(x);
 
-    String myString = new String("this is my first string");
-    String easyString = "easy string";
-    System.out.println(myString + " " + easyString);
+
+    // method named main
+    //     System.out.println("What is your name?");
+
+    // Scanner scanner = new Scanner(System.in); 
+    // String name = scanner.nextLine();
+
+    // System.out.println("hello " + name);
+
+    // String myString = new String("this is my first string");
+    // String easyString = "easy string";
+    // System.out.println(myString + " " + easyString);
+
+    // final int x = 5; //const value; cannot be changed  
 
     }
+
+  
+    
 
     
 }
